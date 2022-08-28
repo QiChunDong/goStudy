679c9ae3bca090cf168f622187b72d9c0ff8bb4d
齐春东 <qichundong@ebchinatech.com>
2022-08-28 17:57:14 +0800

    模拟 curl

--- main.go
+++ main.go
@@ -2,30 +2,49 @@ package main
 
 import (
 	"fmt"
-	pract "gostudy/pract"
-	"gostudy/stu"
-	t "gostudy/tools"
+	"io"
+	"net/http"
 	"os"
 )
+
+// init main之前执行
+func init() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./example2 <url>")
+		os.Exit(-1)
+	}
+}
 
 // main方法
 func main() {
 	fmt.Println("hey world!!!")
 
-	fmt.Println("-----------------BMI  MY TEST CODE")
-	args := os.Args
-	var W float64
-	var H float64
-	fmt.Sscanf(args[1], "%f", &W)
-	fmt.Sscanf(args[2], "%f", &H)
-	t.Printfln("身高，%.2f", H)
-	t.Printfln("体重，%.2f", W)
-	pract.ClacBMI(W, H)
+	// fmt.Println("-----------------BMI  MY TEST CODE")
+	// args := os.Args
+	// var W float64
+	// var H float64
+	// fmt.Sscanf(args[1], "%f", &W)
+	// fmt.Sscanf(args[2], "%f", &H)
+	// t.Printfln("身高，%.2f", H)
+	// t.Printfln("体重，%.2f", W)
+	// pract.ClacBMI(W, H)
 
-	fmt.Println("-----------------int64")
-	stu.TestStruct()
+	// fmt.Println("-----------------int64")
+	// stu.TestStruct()
 
-	fmt.Println("-----------------func")
-	stu.TestFunc()
+	// fmt.Println("-----------------func")
+	// stu.TestFunc()
 
+	// 测试url curl 发送请求
+	r, err := http.Get(os.Args[1])
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// 从body取值
+	io.Copy(os.Stdout, r.Body)
+	if err := r.Body.Close(); err != nil {
+		fmt.Println(err)
+	}
 }
